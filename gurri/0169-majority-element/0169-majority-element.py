62901f1d8b79d8ec8f7e62291a02a9c00a319a77
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-10-18 02:06:13 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ gurri/0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Solution 1. DP
+        counts = collections.defaultdict(int)
+
+        for num in nums:
+            if counts[num] == 0:
+                counts[num] = nums.count(num)
+
+            if counts[num] > len(nums)//2
+                return num
