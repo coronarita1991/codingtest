3132f598f8e15b2d56495efed3bd7f5293b2e374
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-10-18 02:09:06 +0900

    Time: 5 ms (100%), Space: 18.5 MB (34.93%) - LeetHub

--- gurri/0169-majority-element/0169-majority-element.py
+++ gurri/0169-majority-element/0169-majority-element.py
@@ -7,5 +7,5 @@ class Solution:
             if counts[num] == 0:
                 counts[num] = nums.count(num)
 
-            if counts[num] > len(nums)//2
+            if counts[num] > len(nums)//2:
                 return num
