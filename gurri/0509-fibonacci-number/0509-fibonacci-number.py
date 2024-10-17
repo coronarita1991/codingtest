d0bb3006006e879231e98b813bc693931f0ba039
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2024-10-18 03:02:39 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- gurri/0509-fibonacci-number/0509-fibonacci-number.py
+++ gurri/0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,26 @@
 class Solution:
-    def fib(self, n: int) -> int:
-        fibo = [0] * (n+1)
-        # sol1. Tabulation
-        fibo[0] = 0
-        fibo[1] = 1
+    # sol2. Memoization
+    dp = collections.defaultdict(int)
 
-        for i in range(2, n+1):
-            fibo[i] = fibo[i-1] + fibo[i-2]
+    def fib(self, n: int) -> int:
+        if N <= 1:
+            return N
         
-        return fibo[n]
+        if self.dp[N]:
+            return self.dp[N]
+        self.dp[N] = self.fib(N-1) + self.fib(N-2)
+        return self.dp[N]
+    
+        # # sol1. Tabulation
+        # fibo = [0] * (n+1)
+
+        # fibo[0] = 0
+        # if n == 0:
+        #     return fibo[0]
+
+        # fibo[1] = 1
+    
+        # for i in range(2, n+1):
+        #     fibo[i] = fibo[i-1] + fibo[i-2]
+        
+        # return fibo[n]
