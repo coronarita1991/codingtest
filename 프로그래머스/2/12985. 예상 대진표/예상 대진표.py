612e5575ed792f1cf00164e5a5497c71f1ca9118
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2025-02-17 21:39:46 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,17 +1,15 @@
 def solution(n,a,b):
     answer = 1
     
-    if a > b : a, b = b, a # a < b로 생각하고 진행 
-    # offset
+    # if a > b : a, b = b, a # a < b로 생각하고 진행 
+    # # offset
     a -= 1
     b -= 1
     
     while a != b :
-        div_a = a // 2
-        div_b = b // 2
-        if div_a == div_b : 
+        if a // 2 == b // 2 : 
             break
-        a, b = div_a, div_b
+        a, b = a // 2, b // 2
         answer += 1
     
     
