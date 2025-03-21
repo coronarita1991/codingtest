1a3b68c12fea827c275d1b380b7d9f1dd2c07521
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2025-03-21 17:34:02 +0900

    [level 3] Title: 길 찾기 게임, Time: 308.50 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,81 +1,26 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
 
-# Node정의
-class Node:
-    def __init__(self, info, num, left=None, right=None):
-        self.info, self.num, self.left, self.right = info, num, left, right
-        
-    def has_right(self):
-        return self.right is not None
-    
-    def has_left(self):
-        return self.left is not None
-    
-# BT making func
+def VLR_LRV(node,subtree, answer1,answer2):
+    #일단 값을 넣고
+    answer1.append(node[0])
 
-def make_BT(nodeinfo):
-    # 자연수로 노드 만들고
-    nodes = [i for i in range(1, len(nodeinfo)+1)]
-    
-    # Y좌표 내림차순, X좌표 오름차순 으로 노드 번호를 정렬 ** 배워가야 할 점
-    nodes.sort(key=lambda x: (nodeinfo[x-1][1], -nodeinfo[x-1][0]), reverse=True)
-    
-    root = None
-    for i in range(len(nodes)):
-        # 루트 찾고
-        if root is None:
-            root = Node(nodeinfo[nodes[0]-1], nodes[0])
-            
-        else:
-            parent = root
-            node = Node(nodeinfo[nodes[i]-1], nodes[i])
-            
-            while True:
-                # 부모 노드의 X 좌표 크기 비교 / 왼
-                if node.info[0] < parent.info[0]:
-                    if parent.has_left():
-                        parent = parent.left
-                        continue
-                    parent.left = node
-                    break
+    leftsubtree=[_node for _node in subtree if _node[1]<node[1] and _node[2]<node[2]]
+    rightsubtree=[_node for _node in subtree if _node[1]>node[1] and _node[2]<node[2]]
 
-                # 부모노드 X 좌표 커서 오른쪽
-                else:
-                    if parent.has_right():
-                        parent = parent.right
-                        continue
-                    parent.right = node
-                    break
-    return root
-    
-def pre_order(root, answer):
-    stack = [root]
-    while stack:
-        node = stack.pop()
-        # why
-        if node is None:
-            continue
-        answer[0].append(node.num)
-        stack.append(node.right)
-        stack.append(node.left)
+    if leftsubtree!=[]:
+        VLR_LRV(leftsubtree[0],leftsubtree,answer1, answer2)
+    if rightsubtree!=[]:
+        VLR_LRV(rightsubtree[0],rightsubtree,answer1,answer2)
 
-def post_order(root, answer):
-    stack = [(root, False)]
-    while stack:
-        node, visited = stack.pop()
-        if node is None:
-            continue
-        if visited:
-            answer[1].append(node.num)
-        else:
-            stack.append((node, True))
-            stack.append((node.right, False))
-            stack.append((node.left, False))
+    answer2.append(node[0])
 
 def solution(nodeinfo):
-    answer = [[], []]
-    root = make_BT(nodeinfo)
-    # ? 매개변수로 주고 리턴 안받아도 업데이트가 된다. -> mutable객체 내부수정 메서드에 한해서 가능 
-    pre_order(root, answer) 
-    post_order(root, answer) 
+    #nodeinfo에 해당 번호를 넣고, 정렬시키기
+    nodeinfo=[[i+1]+node for i,node in enumerate(nodeinfo)]
+    nodeinfo.sort(key=lambda x:(-x[2],x[1]))
+
+    answer = [[],[]]
+    VLR_LRV(nodeinfo[0],nodeinfo, answer[0],answer[1])
+
     return answer
